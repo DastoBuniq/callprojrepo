68848b7e3185d21b68ce6fe34607ecc822dd03e2
DastoBuniq <damiano.stopo@buniq.it>
2020-10-06 12:56:08 +0200

    Updates

---
+++ precache-manifest.15db7c4bdfaae36940c9fa46a8bc8382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c94c4dfdc1ecc8b3c47090db84d2a9",
+    "url": "/index.html/index.html"
+  },
+  {
+    "revision": "e96582069225935151b2",
+    "url": "/index.html/static/css/main.8ac3b38a.chunk.css"
+  },
+  {
+    "revision": "0367f4ebcf3bf882c7fd",
+    "url": "/index.html/static/js/2.a16247a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/index.html/static/js/2.a16247a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96582069225935151b2",
+    "url": "/index.html/static/js/main.00be70d9.chunk.js"
+  },
+  {
+    "revision": "5bedbd1c79e5cc056cf1",
+    "url": "/index.html/static/js/runtime-main.66791554.js"
+  }
+]);
